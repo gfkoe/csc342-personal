f2ba427e54a95ae18a8dc31fa5c201c74b3a482f
Gabe Koeb <gfkoeb@ncsu.edu>
2023-09-25 21:17:26 -0400

    working on calculator

--- Homework2/js/calculator.js
+++ Homework2/js/calculator.js
@@ -4,5 +4,7 @@ const buttons = document.querySelector(".number");
 buttons.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     //FIXME
+    const key = e.target;
+    const content = key.textContent;
   }
 });
