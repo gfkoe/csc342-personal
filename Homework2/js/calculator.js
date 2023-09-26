f90bbce8f3e383438f98c0b01bea9916e193a1c9
Gabe Koeb <gfkoeb@ncsu.edu>
2023-09-26 12:22:01 -0400

    edit style to calculator

--- Homework2/js/calculator.js
+++ Homework2/js/calculator.js
@@ -1,12 +1,27 @@
-const calculator = document.querySelector(".calculator_body");
-const buttons = document.querySelector(".number");
+console.clear();
+const calculator = document.getElementsByClassName("calculator_body");
+//const buttons = document.querySelector(".number");
 
-buttons.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-    //FIXME
-    const key = e.target;
-    const action = key.dataset.action;
-    const content = key.textContent;
-    const displayed = displayed.textContent;
-  }
-});
+const clear = document.getElementById("clear");
+const decimal = document.getElementById("decimal");
+const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
+const equals = document.getElementById("equals");
+const clearHistory = document.getElementById("clear-history");
+
+let displayed = "50";
+console.log(displayed);
+
+// numbers.addEventListener("click", (e) => {
+//   if (e.target.matches("button")) {
+//     //FIXME
+//     const key = e.target;
+//     const action = key.dataset.action;
+//     const content = key.textContent;
+//     const displayed = displayed.textContent;
+//     console.log(displayed);
+//   }
+//   console.log(displayed);
+// });
+
+// operators.addEventListener("click", (e) => {});
