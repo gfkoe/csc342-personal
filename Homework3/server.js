92f31e1aee00c29b1714e05453c6cbfcbe1b30f1
Gabe Koeb <gfkoeb@ncsu.edu>
2023-10-13 16:08:29 -0400

    updated form

--- Homework3/server.js
+++ Homework3/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ dest: "/static/uploads" });
+const upload = multer({ dest: "static/uploads" });
 
 const app = express();
 const PORT = 3000;
