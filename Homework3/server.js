c3214ae366e6ceec66b7330e1bbd8392b67e737d
Gabe Koeb <gfkoeb@ncsu.edu>
2023-10-13 20:45:47 -0400

    updated form

--- Homework3/server.js
+++ Homework3/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const multer = require("multer");
 
 const app = express();
-const PORT = 3000;
-const html_path = __dirname + "templates/";
+const PORT = 80;
+const html_path = __dirname + "/templates/";
 app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,27 +12,9 @@ app.get("/", (req, res) => {
   res.sendFile(html_path + "form.html");
 });
 
-// upload.single("myfile"),
-app.post("/formdata", (req, res) => {
-  console.log(req.body);
-  if (
-    (req.body.recFirstInput === "Stuart" && req.body.recLastInput == "Dent") ||
-    (req.body.recFirstInput === "Stu" && req.body.recLastInput == "Dent")
-  ) {
-    res.sendFile(html_path + "error.html");
-  } else {
-    res.sendFile(html_path + "success.html");
-  }
-  // try {
-
-  // } catch (err) {
-  //   res.send("Validation failed. " + err);
-  // }
-});
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "static/uploads");
+    cb(null, "static/uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -40,5 +22,35 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+//
+app.post("/formdata", upload.single("imageInput"), (req, res) => {
+  console.log(req.body);
+  if (
+    (req.body.recFirstInput.toLowerCase() == "stuart" &&
+      req.body.recLastInput.toLowerCase() == "dent") ||
+    (req.body.recFirstInput.toLowerCase() == "stu" &&
+      req.body.recLastInput.toLowerCase() == "dent")
+  ) {
+    res.sendFile(html_path + "error.html");
+  } else {
+    res.sendFile(html_path + "success.html");
+  }
+  try {
+    if (req.body.sendFirstInput == "") {
+      throw new Error("sender first name required");
+    }
+    if (req.body.sendLastInput == "") {
+      throw new Error("sender first name required");
+    }
+    if (req.body.recFirstInput == "") {
+      throw new Error("sender first name required");
+    }
+    if (req.body.recLastInput == "") {
+      throw new Error("sender first name required");
+    }
+  } catch (err) {
+    res.send("Validation failed. " + err);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
