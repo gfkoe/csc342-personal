41276b99eb67da383d28f1263ab6b68077cbdb9c
Gabe Koeb <gfkoeb@ncsu.edu>
2023-11-03 20:31:12 -0400

    update login path

--- Homework5/static/js/login.js
+++ Homework5/static/js/login.js
@@ -1,25 +1,25 @@
-import api from './APIClient.js';
+import api from "./APIClient.js";
 
-const loginButton = document.querySelector('#loginButton');
-const username = document.querySelector('#username');
-const password = document.querySelector('#password');
+const loginButton = document.querySelector("#loginButton");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
-const errorBox = document.querySelector('#errorbox');
+const errorBox = document.querySelector("#errorbox");
 
-
-loginButton.addEventListener('click', e => {
-
+loginButton.addEventListener("click", (e) => {
   errorBox.classList.add("hidden");
 
-  api.logIn(username.value, password.value).then(userData => {
-    document.location = "/";
-  }).catch((err) => {
-    errorBox.classList.remove("hidden");
-    if(err.status === 401) {
-      errorBox.innerHTML = "Invalid username or password";
-    }
-    else {
-      errorBox.innerHTML = err;
-    }
-  });
+  api
+    .logIn(username.value, password.value)
+    .then((userData) => {
+      document.location = "/home";
+    })
+    .catch((err) => {
+      errorBox.classList.remove("hidden");
+      if (err.status === 401) {
+        errorBox.innerHTML = "Invalid username or password";
+      } else {
+        errorBox.innerHTML = err;
+      }
+    });
 });
